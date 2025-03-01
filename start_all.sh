ad9e3ad18ca68e11c39126fa689953428106cb4f
Lihuatx <1902561088@qq.com>
2025-03-01 14:01:34 +0800

    commit

--- start_all.sh
+++ start_all.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# 获取脚本所在目录的绝对路径
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR
-
 echo "启动 goods-srv 服务..."
+cd ./goods_srv/target
 ./start.sh
 sleep 1
 
